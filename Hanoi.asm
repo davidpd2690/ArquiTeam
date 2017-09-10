90b0e859cb9982f9f94cfee6901d25b3eeee963e
David Pérez Daher <david.pd2690@gmail.com>
2017-09-10 13:01:18 -0500

    ..

--- Hanoi.asm
+++ Hanoi.asm
@@ -1,21 +1,50 @@
 ##############################################################################################
-#
-#
-#		Práctica 1 - Torres de Hanoi
-#
-#		Equipo: Guillermo , David Pérez Daher
-#
+#											     #
+#											     #
+#		Hanoi Towers						     		     #
+#											     #
+#		Guillermo , David Pérez Daher					             #
+#											     #
+#											     #
 ##############################################################################################
 
 .data
 
-torre1: 
-torre2:
-torre3:
+firstPeg:	  
+auxPeg:		  
+lastPeg:	  
 		
 
 .text
 
-	la $s1, torre1
-	la $s2, torre2
-	la $s3, torre3
+main:
+	
+	la $a1, firstPeg	# first peg - a1
+	la $a2, auxPeg		# aux peg - a2
+	la $a3, lastPeg		# last peg - a3
+	
+	addi $a0, $zero, 6	# number of discs - a0
+	
+	jal stack
+	
+stack:
+	
+	addi $sp, $sp, -20	#save registers in stack
+	sw $a0, 0($sp)
+	sw $a1, 4($sp)
+	sw $a2, 8($sp)
+	sw $a3, 12($sp)
+	sw $ra, 16($sp)
+	
+	sw $a0, 0($a1)
+	
+	beq $a0, 1, one
+	
+one:
+
+moveRing:
+
+
+	
+
+	
