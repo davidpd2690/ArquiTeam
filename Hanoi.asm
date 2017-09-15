e346033edbfa34376158092b29251ffea991f2d9
ROLDAN GOMEZ <IS682431@iteso.mx>
2017-09-15 07:11:48 -0500

    Added a few changes, they can be found on the code comments

--- Hanoi.asm
+++ Hanoi.asm
@@ -3,52 +3,45 @@
 #											     #
 #		Hanoi Towers						     		     #
 #											     #
-#		Guillermo , David Pérez Daher					             #
+#		Guillermo Roldán Gómez, David Pérez Daher			             #
 #											     #
 #											     #
 ##############################################################################################
 
 .data
-
-firstPeg:	.word 0 0 0 0 0 0 0 0	  
-auxPeg:		.word 0 0 0 0 0 0 0 0
-lastPeg:	.word 0 0 0 0 0 0 0 0
+	#added 0s to be able to see how the towers are filled 
+firstPeg: .word 0 0 0 0 0 0 0 0	  
+auxPeg:   .word 0 0 0 0 0 0 0 0  
+lastPeg:  .word 0 0 0 0 0 0 0 0
 		
 
 .text
-
-main:
-	
-	la $t1, firstPeg
-	la $t2, auxPeg
-	la $t3, lastPeg
-	
+	#moved this values out of the main because this are only needed at the beggining
 	la $a1, firstPeg	# first peg - a1
 	la $a2, auxPeg		# aux peg - a2
 	la $a3, lastPeg		# last peg - a3
 	
 	addi $a0, $zero, 6	# number of discs - a0
-	
-	jal stack
-	
-stack:
+main:
 	
 	addi $sp, $sp, -20	#save registers in stack
 	sw $a0, 0($sp)
 	sw $a1, 4($sp)
 	sw $a2, 8($sp)
 	sw $a3, 12($sp)
-	sw $ra, 16($sp)
+	sw $ra, 16($sp)		#ask what is ra used for
 	
 	sw $a0, 0($a1)
 	
-	beq $a0, 1, one
+	beq $a0, 1, one		#compare if the number of disks is not 1, if not jump to one:
+	
+	
+	
+	
+stack:
+	
+	
 	
 one:
 
 moveRing:
-
-
-	
-
-	
