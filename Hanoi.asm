940381a73829bb9a9cbfebb9facde089d1fb4588
David Pérez Daher <david.pd2690@gmail.com>
2017-09-24 15:29:40 -0500

    began recursive algorithm based on c code to move discs from pegs

--- Hanoi.asm
+++ Hanoi.asm
@@ -1,7 +1,7 @@
 ##############################################################################################
 #											     #
 #											     #
-#		Hanoi Towers (Best implementation in the known universe!)		     #
+#		Hanoi Towers								     #
 #											     #
 #		Guillermo Roldán Gómez , David Pérez Daher			   	     #
 #											     #
@@ -27,24 +27,43 @@ main:
 	add $t0, $a0, $zero	# stores number of discs to temp reg t0
 	
 Loop:	#loop to add discs into first peg
-	sw $t0, 0($a1)		# loads number of discs to first Peg
+	sw $t0, 0($a1)		# stores number of discs to first Peg
 	addi $t0, $t0, -1	# number of discs -1
 	addi $a1, $a1, 4	# adding 4 for offset in first Peg address
-	bne $t0, $zero, Loop	# loops until discs == 0
+	bne $t0, $zero, Loop	# loops until all discs are loaded
 	
 	jal hanoi		#jal copies the address of the next instruction into ra, so we need to store it	
 	j exit
 	
 hanoi:
-
+########################################################
+# void hanoi(int n,int com, int aux, int fin){
+#
+#	if(n==1){
+#		printf("%c->%c",com,fin);
+#	}
+#	else{
+#		hanoi(n-1,com,fin,aux);
+#		printf("\n%c->%c\n",com,fin);
+#		hanoi(n-1,aux,com,fin);
+#	}
+#######################################################
 	add $sp, $sp, -8	# reserves space in stack for two regs
 	sw $a0, 0($sp)		# stores number of discs in stack
 	sw $ra, 4($sp)		# stores return address in stack
 	
 	beq $a0,$t9, one	# if number of discs == 1, jump to one	
-	jal moveDiscs		# else jump to moveDiscs
+	
 
 moveDiscs:
+	#hanoi(n-1,com,fin,aux);
+	addi $a0,$a0, -1	# number of discs -1
+	add $t1, $a2, $zero 	# storing value in aux peg to temp reg
+	add $a2, $a3, $zero	# moves discs from last peg to aux peg
+	add $a3, $t1, $zero	# brings back value from temp reg(auxPeg) and moves the discs to last peg
+	jal hanoi		# recursive call back to hanoi
+
+one:
 
 
 exit:
