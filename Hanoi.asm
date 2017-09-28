b612ac0b0a533a207a80668c2b0590bfaef533cf
uriakua <uriakua@gmail.com>
2017-09-28 17:08:43 -0500

    Removed towers from stack in hanoi

--- Hanoi.asm
+++ Hanoi.asm
@@ -3,7 +3,7 @@
 #											     #
 #		Hanoi Towers								     #
 #											     #
-#		Guillermo Roldán Gómez , David Pérez Daher			   	     #
+#		Guillermo RoldÃ¡n GÃ³mez , David PÃ©rez Daher			   	     #
 #											     #
 #											     #
 ##############################################################################################
@@ -23,48 +23,63 @@ main:
 	addi $a3, $zero, 0x10010040		# reserving address for last peg - a3
 	
 	addi $a0, $zero, 6	# number of discs - a0
-	addi $t9, $zero, 1	# loads value 1 for base case comparison
+	addi $t9, $zero, 1	# loads value 1 for base case comparison in temp reg t9
 	add $t0, $a0, $zero	# stores number of discs to temp reg t0
+
 	
+		
+				
 Loop:	#loop to add discs into first peg
 	sw $t0, 0($a1)		# stores number of discs to first Peg
 	addi $t0, $t0, -1	# number of discs -1
 	addi $a1, $a1, 4	# adding 4 for offset in first Peg address
 	bne $t0, $zero, Loop	# loops until all discs are loaded
 	
-	jal hanoi		#jal copies the address of the next instruction into ra, so we need to store it	
+	jal hanoi		#jal copies the address of the next instruction into ra, so we need to store it 
 	j exit
 	
 hanoi:
-########################################################
-# void hanoi(int n,int com, int aux, int fin){
-#
-#	if(n==1){
-#		printf("%c->%c",com,fin);
-#	}
-#	else{
-#		hanoi(n-1,com,fin,aux);
-#		printf("\n%c->%c\n",com,fin);
-#		hanoi(n-1,aux,com,fin);
-#	}
-#######################################################
-	add $sp, $sp, -8	# reserves space in stack for two regs
-	sw $a0, 0($sp)		# stores number of discs in stack
-	sw $ra, 4($sp)		# stores return address in stack
+
+	addi $sp, $sp, -8	# reserves space in stack 
+	sw $ra, 0($sp)		# stores return address in stack
+	sw $a0, 4($sp)		# stores number of discs in stack
 	
-	beq $a0,$t9, one	# if number of discs == 1, jump to one	
+		
+	#beq $a0,$t9, one	# if number of discs == 1, jump to one
+		
 	
 
 moveDiscs:
-	#hanoi(n-1,com,fin,aux);
-	addi $a0,$a0, -1	# number of discs -1
+
 	add $t1, $a2, $zero 	# storing value in aux peg to temp reg
 	add $a2, $a3, $zero	# moves discs from last peg to aux peg
 	add $a3, $t1, $zero	# brings back value from temp reg(auxPeg) and moves the discs to last peg
+	addi $a0,$a0, -1	# number of discs -1
 	jal hanoi		# recursive call back to hanoi
+	
+	lw $ra, 0($sp)		# loads return address in stack
+	lw $a0, 4($sp)		# loads number of discs in stack
+	lw $a1, 8($sp)		# loads firstpeg in stack
+	lw $a2, 12($sp)		# loads auxpeg in stack
+	lw $a3, 16($sp)		# loads lastpeg in stack
+	addi $sp, $sp, 20	# restores space in stack 
 
-one:
-
+moveAgain:
+	
+	addi $sp, $sp, -20	# reserves space in stack 
+	sw $ra, 0($sp)		# stores return address in stack
+	sw $a0, 4($sp)		# stores number of discs in stack
+	sw $a1, 8($sp)		# stores firstpeg in stack
+	sw $a2, 12($sp)		# stores auxpeg in stack
+	sw $a3, 16($sp)		# stores lastpeg in stack
+	
+	add $t2, $a1, $zero	# save firstpeg to temp reg t2
+	add $t3, $a2, $zero	# save auxpeg to temp reg t3
+	add $a1, $a2, $zero	# move from auxpeg to firstpeg
+	add $a2, $t2, $zero	# bring back contents stored in t2(firstpeg) and move to auxpeg
+	
+	addi $a0, $a0, -1	# number of discs -1
+	
+	jr $ra			# back to register address
 
 exit:
-	
